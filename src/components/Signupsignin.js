baefdc188a92f6c4400d44e0c30a8a1d4fb3ffee
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-08-31 11:34:54 -0600

    Register and Sign In

--- src/components/Signupsignin.js
+++ src/components/Signupsignin.js
@@ -1,31 +1,34 @@
 import React, { useState } from 'react'
 
 const Signupsignin = () => {
-  const [action, setAction] = useState();
+  const [action, setAction] = useState("Register");
+
   return (
     <div className="container">
-    <div className="form-box">
-      <h1 id="title">Sign Up</h1>
-      <form action="">
+      <div className="header">
+        <div className="title">{action}</div>
+      </div>
         <div className="input-group">
-          <div className="input-field" id="nameField">
-            <input type="text" placeholder="Name"/>
-          </div>
+        {action === "Log In"? <div></div>: <div className="input-field">
+            <input type="text" placeholder="Name" />
+          </div>}
+          
           <div className="input-field">
             <input type="email" placeholder="Email"/>
           </div>
           <div className="input-field">
             <input type="password" placeholder="Password"/>
           </div>
-          <p>Lost password <a href="#">Click here</a></p>
-        </div>
-        <div className="btn-field">
-          <button type="button" id="SignUpBtn">Sign Up</button>
-          <button type="button" className="disable" id="signInBtn">Sign In</button>
-        </div>
 
-      </form>
-    </div>
+          {action === "Register"? <div></div>: <div className='lost-password'>
+            Lost password <a href="#">Click here</a>
+          </div>}
+          
+        </div>
+        <div className="submit-container">
+          <button className={action === "Log In" ? "submit gray" : "submit"}onClick={()=>{setAction("Register")}}>Register</button>
+          <button className={action === "Register" ? "submit gray" : "submit"} onClick={()=>{setAction("Log In")}}>Log In</button>
+        </div>
     </div>
   )
 }
