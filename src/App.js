e39a215e13771b9ac76f1948de92dc11538dee7d
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-08-30 23:36:47 -0600

    sign up page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="form-box">
+          <h1 id="title">Sign Up</h1>
+          <form action="">
+            <div className="input-group">
+              <div className="input-field" id="nameField">
+                <input type="text" placeholder="Name"/>
+              </div>
+              <div className="input-field">
+                <input type="email" placeholder="Email"/>
+              </div>
+              <div className="input-field">
+                <input type="password" placeholder="Password"/>
+              </div>
+              <p>Lost password <a href="#">Click here</a></p>
+            </div>
+            <div className="btn-field">
+              <button type="button" id="SignUpBtn">Sign Up</button>
+              <button type="button" className="disable" id="signInBtn">Sign In</button>
+            </div>
+
+          </form>
+        </div>
+        </div>
+
     </div>
   );
 }
