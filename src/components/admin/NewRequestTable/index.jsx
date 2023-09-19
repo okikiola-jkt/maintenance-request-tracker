3ce0964aa8209ee5ec089890372d933e6f029f78
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-09-18 19:32:05 -0600

    request details for admin

--- src/components/admin/NewRequestTable/index.jsx
+++ src/components/admin/NewRequestTable/index.jsx
@@ -1,29 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import { Table  } from "react-bootstrap";
+import Offcanvas  from "react-bootstrap/Offcanvas";
+import Button from "react-bootstrap/Button"
 
 
 export const NewRequestTable = () => {
+
+    const [showRequest, setShowRequest] = useState(false);
+    const handleCloseRequest = ()=> setShowRequest(false);
+    const handleShowRequest = () => setShowRequest(true);
+
     return (
         <Table striped>
             <thead>
             <tr>
                 <th>#</th>
-                <th colSpan={2}></th>
+                <th colSpan={2}>Request</th>
             </tr>
             </thead>
             <tbody>
                 <tr>
                     <td>1</td>
-                    <td>Plumbing issue</td>
+                    <td>Plumbing issue</td> 
+                    <div className="d-flex justify-content-center align-items-end">
+                        <Button className="" variant="primary" onClick={handleShowRequest}>
+                            View
+                        </Button>
+                        </div>
+                        <Offcanvas show={showRequest} onHide={handleCloseRequest}>
+                        <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>Request</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            Details of the request
+                        </Offcanvas.Body>
+                        <Button className="m-2">Accept</Button>
+                        <Button className="m-2">Reject</Button>
+                   
+                        </Offcanvas>
+                        
+                    
                 </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Electrical problem</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td colSpan={2}>Garage malfunction</td>
-                </tr>
+              
+                
             </tbody>
         </Table>
     )
