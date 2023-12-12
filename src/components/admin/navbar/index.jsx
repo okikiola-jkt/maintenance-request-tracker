e53d53f4865d4e3551faf58fe74bb83e444a2cf2
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-12-11 19:54:41 -0700

    styled css for active users

--- src/components/admin/navbar/index.jsx
+++ src/components/admin/navbar/index.jsx
@@ -1,15 +1,25 @@
-import React from "react";
-import { Navbar as BNavbar, Container, Nav, Col, Row } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Navbar as BNavbar, Container, Nav, Col, Row } from "react-bootstrap";
 import { useAdminGetAllUsers } from "../../../hooks/mutations/useGetAllUsers";
+import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
 
 export const Navbar = () => {
   const { data, refetch } = useAdminGetAllUsers();
-  const handleActiveUsersClick = () => {
-    refetch();
+  const [showModal, setShowModal] = useState(false);
+  const [activeUsers, setActiveUsers] = useState(null);
+
+  const handleActiveUsersClick = async () => {
+    try {
+      await refetch();
+
+      setActiveUsers(data?.data);
+      setShowModal(true);
+    } catch (error) {}
   };
 
-  let adminToken = localStorage.getItem('adminToken');
+  let adminToken = localStorage.getItem("adminToken");
 
   return (
     <BNavbar expand="lg" className="bg-body-tertiary">
@@ -19,16 +29,19 @@ export const Navbar = () => {
             <BNavbar.Brand href="#home">Maintenance Request</BNavbar.Brand>
           </Col>
           <Col xs={3} className="d-flex justify-content-end align-items-center">
-            <Button variant="primary" onClick={handleActiveUsersClick}>
+            <Button className="me-2 mb-2" onClick={handleActiveUsersClick}>
               Active Users
             </Button>
           </Col>
           {adminToken && (
-            <Col xs={3} className="d-flex justify-content-end align-items-center">
+            <Col
+              xs={3}
+              className="d-flex justify-content-end align-items-center"
+            >
               <BNavbar.Brand
                 href="/admin-login"
                 onClick={() => {
-                  localStorage.removeItem('adminToken');
+                  localStorage.removeItem("adminToken");
                 }}
               >
                 Log out
@@ -38,11 +51,37 @@ export const Navbar = () => {
         </Row>
         <BNavbar.Toggle aria-controls="basic-navbar-nav" />
         <BNavbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            
-          </Nav>
+          <Nav className="me-auto"></Nav>
         </BNavbar.Collapse>
       </Container>
+      <Modal
+        show={showModal}
+        fullscreen={true}
+        onHide={() => setShowModal(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Active Users</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {activeUsers &&
+                activeUsers.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        </Modal.Body>
+      </Modal>
     </BNavbar>
   );
 };
